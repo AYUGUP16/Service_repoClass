0b75797a43ce968603655e06c10c429858cf0832
vikash <vickie0786@gmail.com>
2023-01-09 12:27:04 +0530

    initial installation pushed to repo

---
+++ interface/routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+// Route::get('orders', [OrderController::class, 'index']);
+// Route::get('orders/{id}', [OrderController::class, 'show']);
+// Route::post('orders', [OrderController::class, 'store']);
+// Route::put('orders/{id}', [OrderController::class, 'update']);
+// Route::delete('orders/{id}', [OrderController::class, 'delete']);
+
